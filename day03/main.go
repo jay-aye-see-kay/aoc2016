c5b121ad12d72bf9d293ed7e1dad3a0f029d63ed
Jack Rose <user@jackrose.co.nz>
2022-11-24 09:34:29 +1100

    day 3 part 2

--- day03/main.go
+++ day03/main.go
@@ -15,6 +15,7 @@ func main() {
 	text := strings.TrimSpace(string(content))
 
 	fmt.Println("part 1:", part1(text))
+	fmt.Println("part 2:", part2(text))
 }
 
 // ans: 993
@@ -28,11 +29,33 @@ func part1(text string) int {
 	return len(validTriangles)
 }
 
+// ans: 1849
+func part2(text string) int {
+	var validTriangles [][]int
+	for _, triangle := range transposeTriangles(parseInput(text)) {
+		if isValidTriangle(triangle) {
+			validTriangles = append(validTriangles, triangle)
+		}
+	}
+	return len(validTriangles)
+}
+
+func transposeTriangles(t [][]int) [][]int {
+	var transposed [][]int
+	for i := 0; i < len(t); i += 3 {
+		row1 := []int{t[i][0], t[i+1][0], t[i+2][0]}
+		row2 := []int{t[i][1], t[i+1][1], t[i+2][1]}
+		row3 := []int{t[i][2], t[i+1][2], t[i+2][2]}
+		transposed = append(transposed, row1, row2, row3)
+	}
+	return transposed
+}
+
 func isValidTriangle(sides []int) bool {
-  valid1 := sides[0] + sides[1] > sides[2]
-  valid2 := sides[1] + sides[2] > sides[0]
-  valid3 := sides[2] + sides[0] > sides[1]
-  return valid1 && valid2 && valid3
+	valid1 := sides[0]+sides[1] > sides[2]
+	valid2 := sides[1]+sides[2] > sides[0]
+	valid3 := sides[2]+sides[0] > sides[1]
+	return valid1 && valid2 && valid3
 }
 
 func parseInput(text string) [][]int {
